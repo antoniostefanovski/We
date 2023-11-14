2f47e5254b74ad022d6330d5eb940f1f2bad2e48
Antonio <antonio.stefanovski@yahoo.com>
2023-11-14 15:23:34 +0100

    thymeleaf fix

--- laboratory2/src/main/java/mk/ukim/finki/wp/laboratory1/web/controller/MovieController.java
+++ laboratory2/src/main/java/mk/ukim/finki/wp/laboratory1/web/controller/MovieController.java
@@ -22,10 +22,7 @@ public class MovieController {
     @GetMapping
     public String getMoviesPage(@RequestParam(required = false) String error, Model model) {
         model.addAttribute("movies", movieService.listAll());
-        
-        if(error != null && !error.isEmpty()) {
-            model.addAttribute("error", error);
-        }
+        model.addAttribute("hasError", false);
 
         return "listMovies";
     }
@@ -47,7 +44,7 @@ public class MovieController {
 
             return "/add-movie";
         }
-        return "redirect:/movies?error=A movie with the given id does not exist!";
+        return "redirect:/movies";
     }
 
     @PostMapping("/add-movie")
