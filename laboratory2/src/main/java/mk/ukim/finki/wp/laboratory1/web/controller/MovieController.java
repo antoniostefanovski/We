e2f432a0479089ab3d1b7c47a538ad26980fab3a
Antonio <antonio.stefanovski@yahoo.com>
2023-11-14 20:27:14 +0100

    fix labs

--- laboratory2/src/main/java/mk/ukim/finki/wp/laboratory1/web/controller/MovieController.java
+++ laboratory2/src/main/java/mk/ukim/finki/wp/laboratory1/web/controller/MovieController.java
@@ -22,7 +22,10 @@ public class MovieController {
     @GetMapping
     public String getMoviesPage(@RequestParam(required = false) String error, Model model) {
         model.addAttribute("movies", movieService.listAll());
-        model.addAttribute("hasError", false);
+
+        if(error != null && !error.isEmpty()) {
+            model.addAttribute("error", error);
+        }
 
         return "listMovies";
     }
@@ -44,7 +47,7 @@ public class MovieController {
 
             return "/add-movie";
         }
-        return "redirect:/movies";
+        return "redirect:/movies?error=A movie with the given id does not exist!";
     }
 
     @PostMapping("/add-movie")
@@ -59,7 +62,7 @@ public class MovieController {
         return "redirect:/movies";
     }
 
-    @PostMapping("/delete-movie/{id}")
+    @GetMapping("/delete-movie/{id}")
     public String deleteProduct(@PathVariable Long id) {
         this.movieService.deleteById(id);
         return "redirect:/movies";
